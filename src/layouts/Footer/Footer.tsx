5a081912d8feb9b2efbafa6136863953ea7dac65
arditmirashi2 <arditmirashi5@gmail.com>
2022-06-03 14:12:46 +0200

    Removed unused files

--- src/layouts/Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface FooterProps {
-  children: any;
-}
-
-export const Footer: React.FunctionComponent<FooterProps> = props => {
-  return <footer>{props.children}</footer>;
-};
--- src/layouts/Header/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface HeaderProps {
-  children: any;
-}
-
-export const Header: React.FunctionComponent<HeaderProps> = props => {
-  return <header>{props.children}</header>;
-};
